6b3d8eab47529004a5b7dd61407d83e021a77c3c
zyndev <zyndev@gmail.com>
2017-08-02 17:10:27 +0800

    添加 oauth 模块

---
+++ spring-boot-oauth/src/main/java/com/zyndev/springbootoauth/SpringBootOauthApplication.java
@@ -0,0 +1,12 @@
+package com.zyndev.springbootoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauthApplication.class, args);
+	}
+}
