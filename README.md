c28a53b818b5d13d69120203677eaba0477f8729
zyndev <zyndev@gmail.com>
2017-08-02 19:10:04 +0800

    添加其它模块

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 #zyndev-spring-cloud
 学习 spring-cloud
+
+### 编写顺序：
+
+1. spring-boot-demo
+
+    > 初体验，从 Spring MVC 向 Spring boot 迁移 
+
+2. spring-boot-mybatis
+
+    > 配置 mybatis
+
+3. spring-boot-redis
+
+    > 配置 redis
+
+4. spring-boot-oauth
+
+    > 简单实现 oauth 验证
+
+5. spring-boot-sso 
+
+    > 简单实现 sso 单点登录
---
+++ spring-boot-sso/src/main/java/com/zyndev/springbootsso/SpringBootSsoApplication.java
@@ -0,0 +1,12 @@
+package com.zyndev.springbootsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSsoApplication.class, args);
+	}
+}
