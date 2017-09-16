7e8c579d4b3da418dd9e596b2f5a0b3735461807
张瑀楠 <zyndev@gmail.com>
2017-09-16 21:06:17 +0800

    好想写点什么

---
+++ spring-boot-eureka-client/src/main/java/com/zyndev/springbooteurekaclient/SpringBootEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.zyndev.springbooteurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEurekaClientApplication.class, args);
+	}
+}
