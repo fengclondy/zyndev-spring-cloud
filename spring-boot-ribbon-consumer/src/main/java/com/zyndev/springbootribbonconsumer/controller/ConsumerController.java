1d0218dfc84327c663626982eb69b154c81ab663
zyndev <zyndev@gmail.com>
2017-09-28 12:13:37 +0800

    delete springBootRibbonConsumer unnecessary dependency and optimize imports

--- spring-boot-ribbon-consumer/src/main/java/com/zyndev/springbootribbonconsumer/controller/ConsumerController.java
+++ spring-boot-ribbon-consumer/src/main/java/com/zyndev/springbootribbonconsumer/controller/ConsumerController.java
@@ -2,7 +2,8 @@ package com.zyndev.springbootribbonconsumer.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -19,28 +20,23 @@ public class ConsumerController {
     private RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "ribbonConsumerFallBack")
-    @RequestMapping("/ribbon-consumer")
-    public String helloConsumer(@RequestParam("key")String key) {
-        System.out.println("key " + key);
+    @RequestMapping("/hello-hello")
+    public String helloConsumer() {
         return restTemplate.getForEntity("http://HELLO-SERVER/hello", String.class).getBody();
     }
 
-    public String ribbonConsumerFallBack(String key) {
+    public String ribbonConsumerFallBack() {
         return "{'result':200,'timeout':100}";
     }
 
-    @RequestMapping("/ribbon-consumer-python")
-    public String pythonConsumer() {
-        return restTemplate.getForEntity("http://WEBSOCKET-SERVER/tttt", String.class).getBody();
+    @RequestMapping("/python-hello")
+    public String pythonHelloConsumer() {
+        return restTemplate.getForEntity("http://WEBSOCKET-SERVER/hello", String.class).getBody();
     }
 
-    @ResponseBody
-    @RequestMapping("/send/{appId}/{userId}")
-    public String sendPython(@PathVariable("appId")String appId, @PathVariable("userId")String userId) {
-        System.out.println(" appId:" + appId + "  userId:" + userId);
-        return restTemplate.postForEntity("http://WEBSOCKET-SERVER/send/"+ appId +"/" + userId, null ,String.class).getBody();
+    @RequestMapping("/python-sidecar")
+    public String pythonSideCarConsumer() {
+        return restTemplate.getForEntity("http://WEBSOCKET-SERVER/sidecar", String.class).getBody();
     }
-
-
 
 }
