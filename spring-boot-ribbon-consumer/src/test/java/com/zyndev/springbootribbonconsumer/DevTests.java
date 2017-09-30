26c8e3b034836adce30a84e40e6a2e77e00496a4
zyndev <zyndev@gmail.com>
2017-09-30 14:48:31 +0800

    add a test

---
+++ spring-boot-ribbon-consumer/src/test/java/com/zyndev/springbootribbonconsumer/DevTests.java
@@ -0,0 +1,17 @@
+package com.zyndev.springbootribbonconsumer;
+
+import org.junit.Test;
+
+/**
+ * Desc:
+ * Author: 张瑀楠 wb.yunan.zhang@renren-inc.com
+ * TODO:
+ */
+public class DevTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("张瑀楠 zyndev@gmail.com");
+    }
+
+}
